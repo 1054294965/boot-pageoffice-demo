72b8fa8aa760b28d84a3c4d871abb9bad27bfa1a
chushiyun <1054294965@qq.com>
2019-11-08 10:27:31 +0800

    111

---
+++ src/main/java/com/example/pageofficedemo/PageOfficeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.pageofficedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageOfficeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageOfficeDemoApplication.class, args);
+    }
+}
